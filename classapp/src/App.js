a104ef126584cfd495fdd23c19398f0da4d7d994
Sanpreet Singh <sanpreet.india@gmail.com>
2023-05-21 19:49:36 +0530

    WIP: learning the props

--- classapp/src/App.js
+++ classapp/src/App.js
@@ -2,28 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 import Header from './components/Header';
+import Contactus from './components/ContactUs'
 
-function App() {
-  return (
-    <div className="">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+// function App() {
+//   return (
+//     <div className="">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//       <Header></Header>
+//       <Hello></Hello>
+//       <Contactus></Contactus>
+//     </div>
+//   );
+// }
+
+
+function App(){
+  const contactdetails = {
+    name: "San",
+    phone: "99999999",
+    email: "san@test.com"
+
+  }
+  const salutation = ["Hello", "Namaste", "Ola", "hola"]
+  return(
+    <div>
       <Header></Header>
-      <Hello></Hello>
+      <Contactus name={contactdetails.name} phone={contactdetails.phone} email={contactdetails.email} salutation={contactdetails.salutation}>
+
+      </Contactus>
     </div>
   );
+
+
 }
 
 export default App;
---
+++ classapp/src/components/ContactUs.js
@@ -0,0 +1,17 @@
+import React from "react"
+// import Hello from "./hello"
+
+export default function Contactus(props){
+    console.log(props.salutation)
+
+    return(<>
+        Name: Sanpreet<br></br>
+        Phone: 999999999<br></br>
+        email: test@test.com<br></br>
+        </>)
+
+}
+
+
+
+//export default Contactus
